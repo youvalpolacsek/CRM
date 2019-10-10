05d9e082d962acbbe27db3bbe510eca3c3d2f309
Youval Polacsek <Youval@h-MacBook-Pro-sl-Youval.local>
2019-10-10 14:45:57 +0300

    fixed issues

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express()
-const data = require ('../react-crm-ex-youvalpolacsek/data')
+// const data = require ('../react-crm-ex-youvalpolacsek/data')
 let Client = require ('./ClientModel')
 
 mongoose.connect("mongodb://127.0.0.1/CRMDB", { useNewUrlParser: true })
